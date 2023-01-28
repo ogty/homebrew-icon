e06053c785034aa327421fd35a9eedc2c7226bc2
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2023-01-29 00:37:22 +0900

    update

--- Formula/icon.rb
+++ Formula/icon.rb
@@ -1,11 +1,21 @@
 class Icon < Formula
   desc ""
   homepage "https://github.com/ogty/icon"
-  url "https://github.com/ogty/icon/releases/download/v0.0.1/icon-0.0.1-x86_64-apple-darwin.tar.gz"
-  sha256 "1f54ed0f6956c1802be69412c41974bdcfaf5b02de1d8b13b0f9fcecff958a35"
-  version "0.0.1"
+  url "https://github.com/ogty/icon/releases/download/v0.0.2/icon-0.0.2-x86_64-apple-darwin.tar.gz"
+  sha256 "43b9031fb39dd4095ec3e78e474f2f27b0fc0198dff10d3d46325434789f64fa"
+  version "0.0.2"
+
+  depends_on "librsvg"
 
   def install
     bin.install "icon"
+  end
+
+  def post_install
+    system "#{bin}/icon", "init"
+  end
+
+  def uninstall
+    bin.uninstall "icon"
   end
 end
